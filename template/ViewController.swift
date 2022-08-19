de36c1010da6e2b9227d8bd63c3947d0bc7c9a6c
Jason Prasad <jasongprasad@gmail.com>
2022-08-19 14:12:44 -0400

    Remove unnecessary code and comments

--- template/ViewController.swift
+++ template/ViewController.swift
@@ -1,19 +1,7 @@
-//
-//  ViewController.swift
-//  template
-//
-//  Created by Jason Prasad on 8/19/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
